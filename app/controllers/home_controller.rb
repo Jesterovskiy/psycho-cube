9e0259d4df30383535d76cf6bfc3a3bd46ca86bc
Jester <jester@aejis.eu>
2014-11-17 15:41:23 +0200

    Add elements to home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,12 @@
 class HomeController < ApplicationController
+  require "prawn"
+
   def index
+  end
+
+  def generate_cube
+    Prawn::Document.generate("hello.pdf") do
+      text "Hello World!"
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  get 'home/index'          => 'home#index'
+  post 'home/generate_cube' => 'home#generate_cube'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
